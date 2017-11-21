ae203b887d58d25dec2303d135f8e4474b8a6354
xilingit <1026771640@qq.com>
2017-11-21 16:24:55 +0800

    添加自定义linearlayout组合控件

--- app/src/main/java/com/xilin/MainActivity.java
+++ app/src/main/java/com/xilin/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     private Button customView, customView2;
 
     @Override
