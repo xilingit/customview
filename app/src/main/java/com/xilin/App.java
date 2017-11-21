f5d4065b546c2fc2454905c008bd303db7095a71
xilingit <1026771640@qq.com>
2017-11-21 13:45:13 +0800

    首次提交

---
+++ app/src/main/java/com/xilin/App.java
@@ -0,0 +1,15 @@
+package com.xilin;
+
+import android.app.Application;
+
+/**
+ * Created by hehe on 2017/11/21.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
---
+++ app/src/main/java/com/xilin/MainActivity.java
@@ -0,0 +1,35 @@
+package com.xilin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button customView, customView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        customView = (Button) findViewById(R.id.btn_customview);
+        customView2 = (Button) findViewById(R.id.btn_customview2);
+        customView.setOnClickListener(this);
+        customView2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_customview:
+                startActivity(new Intent(MainActivity.this,CustomViewAct.class));
+                break;
+            case R.id.btn_customview2:
+                break;
+            default:
+                break;
+        }
+    }
+}
